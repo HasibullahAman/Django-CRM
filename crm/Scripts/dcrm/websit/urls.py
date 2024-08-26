8963c30dfb62f04d99802da92635dba6570f5392
Hasibullah Aman <hasib.aman7@gmail.com>
2024-08-26 11:14:15 +0430

    last day

--- crm/Scripts/dcrm/websit/urls.py
+++ crm/Scripts/dcrm/websit/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home', views.home , name = 'home'),
+    # path('login/', views.login_user , name = 'login'),
+    # path('logout', views.logout_user , name = 'logout'),
+    
 ]
