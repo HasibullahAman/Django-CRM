cc019ff9297556cf80343d45f239d1a19a245fd3
HasibullahAman <hasib.aman7@gmail.com>
2026-04-07 11:29:04 +0430

    uderstanding data model

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 # Create your views here.
-
-
 def say_hello(request):
-    return HttpResponse("Hello World")
+    x = calculate()    
+    return render(request, "hello.html", {"name": "Hasibullah Aman"})
